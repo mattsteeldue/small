f43e550211faeb85a454b358b6ff6bc2e95a505a
Matteo Vitturi <44555162+mattsteeldue@users.noreply.github.com>
2021-09-04 23:07:31 +0200

    Commit

---
+++ asort/src/asort.php
@@ -0,0 +1,23 @@
+    <?php 
+    
+        $data = $_GET["DATA"] ;
+        
+        $ary = explode( ',', $data ) ;
+        sort( $ary ) ;
+        
+        $output = array( ) ;
+        
+        $i = 0 ;
+        while ( count($ary) ) {
+            array_push( $output, array_pop( $ary ) ) ;
+            if ( count($ary) ) {
+                array_push( $output, array_shift( $ary ) ) ;
+            }
+        }
+        
+        $result = implode(',', $output) ;
+
+        echo "$result" ;
+        
+        
+    ?>
